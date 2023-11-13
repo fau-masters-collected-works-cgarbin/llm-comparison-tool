09016b2a10a8ff677e1d5ca03f2f0efa72dab678
Christian Garbin <garbin.christian@gmail.com>
2023-11-13 05:25:45 -0500

    Move configuration to session state and collapsible section

--- app.py
+++ app.py
@@ -11,30 +11,43 @@ def get_models():
 
 st.set_page_config(page_title="LLM Selector", layout="wide")
 
-# Sidebar for model selection and temperature
-models = get_models()
-selected_models = st.sidebar.multiselect("Choose LLM(s)", models)
-temperature = st.sidebar.slider("Select temperature", 0.0, 2.0, 0.0)
 
-with st.expander("Prompt"):
-    prompt = st.text_area("Prompt", height=200)
+def configuration():
+    with st.expander("Click to configure prompt and models"):
+        if "key" not in st.session_state:
+            st.session_state.key = ""
+        if "prompt" not in st.session_state:
+            st.session_state.prompt = ""
+        if "temperature" not in st.session_state:
+            st.session_state.temperature = 0.0
+        if "models" not in st.session_state:
+            st.session_state.models = []
+        models = get_models()
+        st.session_state.prompt = st.text_area("Prompt", height=200)
+        st.session_state.temperature = st.slider("Select temperature", 0.0, 2.0, 0.0)
+        st.session_state.models = st.multiselect("Choose LLM(s)", models)
+
+
+configuration()
 
 # Input field for the request and send button
 user_input = st.text_area("Enter your request to the LLM", height=150)
 send_button = st.button("Send Request")
 
+st.write(f"Selected models: {', '.join([model.name for model in st.session_state.models])}")
+
 # Send request and display response
 if send_button:
     with st.spinner("Sending request..."):
-        model = selected_models[0]
-        response = llm.chat_completion(model.id, prompt, user_input, temperature)
+        model = st.session_state.models[0]
+        response = llm.chat_completion(model.id, st.session_state.prompt, user_input, st.session_state.temperature)
     with st.spinner("Calculating cost and stats..."):
         cost_and_stats = llm.cost_and_stats(response)
 
     st.markdown(f"### {model.name} response")
     with st.expander("Click to see the raw response"):
-        st.write("#### LLM raw response")
+        st.write("LLM raw response")
         st.json(response.raw_response, expanded=False)
-        st.write("#### Cost and stats raw response")
+        st.write("Cost and stats raw response")
         st.json(cost_and_stats.raw_response, expanded=False)
     st.markdown(response.response)
