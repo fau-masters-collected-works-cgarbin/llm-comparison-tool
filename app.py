8e876cc1a93f7041f18efccbe84a9acac19f8256
Christian Garbin <garbin.christian@gmail.com>
2023-11-15 08:13:04 -0500

    Make configuration options more visible

--- app.py
+++ app.py
@@ -25,28 +25,27 @@ def prepare_session_state():
 
 
 def configuration():
-    with st.expander("Click to to show/hide configuration options (system prompt, temperature, models)"):
-        models = get_models()
-        st.session_state.prompt = st.text_area(
-            "System prompt",
-            value="You are a helpful assistant.",
-            placeholder="Enter here the system prompt",
-            height=150,
-        )
-        cols = st.columns([5, 2, 1])
-        with cols[0]:
-            st.session_state.models = st.multiselect("Model(s)", models, placeholder="Select one or more models")
-        with cols[1]:
-            st.session_state.temperature = st.slider("Temperature", 0.0, 2.0, 0.0)
-        with cols[2]:
-            st.session_state.max_tokens = st.number_input("Max completion tokens", 1, 20_480, 2048, step=10)
+    models = get_models()
+    st.session_state.prompt = st.text_area(
+        "System prompt",
+        value="You are a helpful assistant.",
+        placeholder="Enter here the system prompt",
+        height=120,
+    )
+    cols = st.columns([5, 2, 1])
+    with cols[0]:
+        st.session_state.models = st.multiselect("Model(s)", models, placeholder="Select one or more models")
+    with cols[1]:
+        st.session_state.temperature = st.slider("Temperature", 0.0, 2.0, 0.0)
+    with cols[2]:
+        st.session_state.max_tokens = st.number_input("Max completion tokens", 1, 20_480, 2048, step=10)
 
-        # Models are listed in the order the user selected them
-        # Sort the selected list by name to make them easier to find in the results
-        st.session_state.models = sorted(st.session_state.models, key=lambda x: x.name)
+    # Models are listed in the order the user selected them
+    # Sort the selected list by name to make them easier to find in the results
+    st.session_state.models = sorted(st.session_state.models, key=lambda x: x.name)
 
-        # Show all modes in a markdown table
-        st.markdown("**Models details**")
+    # Show all modes in a markdown table
+    with st.expander("Click to to show/hide model details"):
         model_list = (
             "| Model | ID | Prompt price | Completion price |"
             "Context length | Max completion tokens | Tokenizer | Instruct type\n"
@@ -59,12 +58,6 @@ def configuration():
                 f"{model.instruct_type} |\n"
             )
         st.markdown(model_list)
-    selected_models = (
-        ", ".join([model.name for model in st.session_state.models])
-        if st.session_state.models
-        else "_Click above to select models_"
-    )
-    st.write(f"Selected models: {selected_models}")
 
 
 def get_llm_response(user_input: str) -> dict[llm.Model, llm.LLMResponse]:
